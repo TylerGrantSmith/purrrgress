b9c4daaa6d7a969804a844bb0d7e9cef4c8b31ea
Tyler Smith <tyler.smith@fbfs.com>
2019-09-30 10:26:26 -0500

    fixed spacing and formals

--- R/progressively.R
+++ R/progressively.R
@@ -1,6 +1,7 @@
 make_progress <- function(total = 100,
                           format = "(:current/:total) [:bar] :percent [Elapsed: :elapsed ETA: :eta]",
-                          clear = FALSE,  show_after = 0,
+                          clear = FALSE,
+                          show_after = 0,
                           width = getOption("width")) {
 
   progress::progress_bar$new(
@@ -35,7 +36,6 @@ make_progress <- function(total = 100,
 }
 
 .pbify <- function(.mapper, .args) {
-  fmls <- rlang::fn_fmls(.mapper)
   pf <- function(...) {
     args <- .make_args(as.list(match.call())[-1], .args, rlang::caller_env())
 
@@ -50,7 +50,7 @@ make_progress <- function(total = 100,
     do.call(.mapper, args, envir = rlang::caller_env())
   }
 
-  formals(pf) <- formals(.mapper)
+  rlang::fn_fmls(pf) <- rlang::fn_fmls(.mapper)
   pf
 }
 
@@ -100,13 +100,13 @@ progressively <- function(.mapper, .farg = NULL, .xarg = NULL,  .yarg = NULL, .l
 #'
 #' @inheritParams purrr::map
 #' @examples
-pro_map    <- progressively(purrr::map, .farg = '.f', .xarg = '.x')
+pro_map <- progressively(purrr::map, .farg = '.f', .xarg = '.x')
 #' @rdname pro_map
 #' @export
-pro_map2   <- progressively(purrr::map2, .farg = '.f', .xarg = '.x', .yarg = '.y')
+pro_map2 <- progressively(purrr::map2, .farg = '.f', .xarg = '.x', .yarg = '.y')
 #' @rdname pro_map
 #' @export
-pro_pmap   <- progressively(purrr::pmap, .farg = '.f', .larg = '.l')
+pro_pmap <- progressively(purrr::pmap, .farg = '.f', .larg = '.l')
 #' @rdname pro_map
 #' @export
 pro_map_if <- progressively(purrr::map_if, .farg = '.f', .xarg = '.x', .parg = '.p')
@@ -115,37 +115,37 @@ pro_map_if <- progressively(purrr::map_if, .farg = '.f', .xarg = '.x', .parg = '
 pro_map_at <- progressively(purrr::map_at, .farg = '.f', .xarg = '.x', .atarg = '.at')
 #' @rdname pro_map
 #' @export
-pro_map  <- progressively(purrr::map, .farg = '.f', .xarg = '.x' )
+pro_map <- progressively(purrr::map, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
 pro_map_at <- progressively(purrr::map_at, .farg = '.f', .xarg = '.x', .atarg = '.at')
 #' @rdname pro_map
 #' @export
-pro_map_chr  <- progressively(purrr::map_chr, .farg = '.f', .xarg = '.x' )
+pro_map_chr <- progressively(purrr::map_chr, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_map_dbl  <- progressively(purrr::map_dbl, .farg = '.f', .xarg = '.x' )
+pro_map_dbl <- progressively(purrr::map_dbl, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_map_df <- progressively(purrr::map_df, .farg = '.f', .xarg = '.x'  )
+pro_map_df <- progressively(purrr::map_df, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_map_dfc  <- progressively(purrr::map_dfc, .farg = '.f', .xarg = '.x' )
+pro_map_dfc <- progressively(purrr::map_dfc, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_map_dfr  <- progressively(purrr::map_dfr, .farg = '.f', .xarg = '.x' )
+pro_map_dfr <- progressively(purrr::map_dfr, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
 pro_map_if <- progressively(purrr::map_if, .farg = '.f', .xarg = '.x', .parg ='.p')
 #' @rdname pro_map
 #' @export
-pro_map_int  <- progressively(purrr::map_int, .farg = '.f', .xarg = '.x' )
+pro_map_int <- progressively(purrr::map_int, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_map_lgl  <- progressively(purrr::map_lgl, .farg = '.f', .xarg = '.x' )
+pro_map_lgl <- progressively(purrr::map_lgl, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_walk <- progressively(purrr::walk, .farg = '.f',  .xarg = '.x')
+pro_walk <- progressively(purrr::walk, .farg = '.f', .xarg = '.x')
 #' @rdname pro_map
 #' @export
 pro_map2 <- progressively(purrr::map2, .farg = '.f', .xarg = '.x', .yarg = '.y')
@@ -157,7 +157,7 @@ pro_map2_chr <- progressively(purrr::map2_chr, .farg = '.f', .xarg = '.x', .yarg
 pro_map2_dbl <- progressively(purrr::map2_dbl, .farg = '.f', .xarg = '.x', .yarg = '.y')
 #' @rdname pro_map
 #' @export
-pro_map2_df  <- progressively(purrr::map2_df,  .farg = '.f', .xarg = '.x', .yarg = '.y')
+pro_map2_df <- progressively(purrr::map2_df, .farg = '.f', .xarg = '.x', .yarg = '.y')
 #' @rdname pro_map
 #' @export
 pro_map2_dfc <- progressively(purrr::map2_dfc, .farg = '.f', .xarg = '.x', .yarg = '.y')
@@ -172,34 +172,34 @@ pro_map2_int <- progressively(purrr::map2_int, .farg = '.f', .xarg = '.x', .yarg
 pro_map2_lgl <- progressively(purrr::map2_lgl, .farg = '.f', .xarg = '.x', .yarg = '.y')
 #' @rdname pro_map
 #' @export
-pro_walk2  <- progressively(purrr::walk2, .farg = '.f', .xarg = '.x', .yarg = '.y' )
+pro_walk2 <- progressively(purrr::walk2, .farg = '.f', .xarg = '.x', .yarg = '.y' )
 #' @rdname pro_map
 #' @export
-pro_imap     <- progressively(purrr::imap, .farg = '.f', .xarg = '.x')
+pro_imap   <- progressively(purrr::imap, .farg = '.f', .xarg = '.x')
 #' @rdname pro_map
 #' @export
-pro_imap_chr <- progressively(purrr::imap_chr, .farg = '.f', .xarg = '.x'  )
+pro_imap_chr <- progressively(purrr::imap_chr, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_imap_dbl <- progressively(purrr::imap_dbl, .farg = '.f', .xarg = '.x'  )
+pro_imap_dbl <- progressively(purrr::imap_dbl, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_imap_dfc <- progressively(purrr::imap_dfc, .farg = '.f', .xarg = '.x'  )
+pro_imap_dfc <- progressively(purrr::imap_dfc, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_imap_dfr <- progressively(purrr::imap_dfr, .farg = '.f', .xarg = '.x'  )
+pro_imap_dfr <- progressively(purrr::imap_dfr, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_imap_int <- progressively(purrr::imap_int, .farg = '.f', .xarg = '.x'  )
+pro_imap_int <- progressively(purrr::imap_int, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_imap_lgl <- progressively(purrr::imap_lgl, .farg = '.f', .xarg = '.x'  )
+pro_imap_lgl <- progressively(purrr::imap_lgl, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_iwalk    <- progressively(purrr::iwalk, .farg = '.f', .xarg = '.x' )
+pro_iwalk <- progressively(purrr::iwalk, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_invoke_map <- progressively(purrr::invoke_map, .farg = '.f', .xarg = '.x'  )
+pro_invoke_map <- progressively(purrr::invoke_map, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
 pro_invoke_map_chr <- progressively(purrr::invoke_map_chr, .farg = '.f', .xarg = '.x')
@@ -208,7 +208,7 @@ pro_invoke_map_chr <- progressively(purrr::invoke_map_chr, .farg = '.f', .xarg =
 pro_invoke_map_dbl <- progressively(purrr::invoke_map_dbl, .farg = '.f', .xarg = '.x')
 #' @rdname pro_map
 #' @export
-pro_invoke_map_df  <- progressively(purrr::invoke_map_df,  .farg = '.f', .xarg = '.x')
+pro_invoke_map_df <- progressively(purrr::invoke_map_df, .farg = '.f', .xarg = '.x')
 #' @rdname pro_map
 #' @export
 pro_invoke_map_dfc <- progressively(purrr::invoke_map_dfc, .farg = '.f', .xarg = '.x')
@@ -223,13 +223,13 @@ pro_invoke_map_int <- progressively(purrr::invoke_map_int, .farg = '.f', .xarg =
 pro_invoke_map_lgl <- progressively(purrr::invoke_map_lgl, .farg = '.f', .xarg = '.x')
 #' @rdname pro_map
 #' @export
-pro_lmap <- progressively(purrr::lmap, .farg = '.f', .xarg = '.x'  )
+pro_lmap <- progressively(purrr::lmap, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
-pro_lmap_at  <- progressively(purrr::lmap_at, .farg = '.f', .xarg = '.x', .atarg = '.at')
+pro_lmap_at <- progressively(purrr::lmap_at, .farg = '.f', .xarg = '.x', .atarg = '.at')
 #' @rdname pro_map
 #' @export
-pro_lmap_if  <- progressively(purrr::lmap_if, .farg = '.f', .xarg = '.x', .parg  = '.p')
+pro_lmap_if <- progressively(purrr::lmap_if, .farg = '.f', .xarg = '.x', .parg = '.p')
 #' @rdname pro_map
 #' @export
 pro_modify <- progressively(purrr::modify, .farg = '.f', .xarg = '.x' )
@@ -244,7 +244,7 @@ pro_pmap_chr <- progressively(purrr::pmap_chr, .farg = '.f', .larg = '.l')
 pro_pmap_dbl <- progressively(purrr::pmap_dbl, .farg = '.f', .larg = '.l')
 #' @rdname pro_map
 #' @export
-pro_pmap_df  <- progressively(purrr::pmap_df, .farg = '.f' , .larg = '.l')
+pro_pmap_df <- progressively(purrr::pmap_df, .farg = '.f' , .larg = '.l')
 #' @rdname pro_map
 #' @export
 pro_pmap_dfc <- progressively(purrr::pmap_dfc, .farg = '.f', .larg = '.l')
@@ -259,7 +259,7 @@ pro_pmap_int <- progressively(purrr::pmap_int, .farg = '.f', .larg = '.l')
 pro_pmap_lgl <- progressively(purrr::pmap_lgl, .farg = '.f', .larg = '.l')
 #' @rdname pro_map
 #' @export
-pro_pwalk  <- progressively(purrr::pwalk, .farg = '.f', .larg = '.l')
+pro_pwalk <- progressively(purrr::pwalk, .farg = '.f', .larg = '.l')
 #' @rdname pro_map
 #' @export
 pro_reduce <- progressively(purrr::reduce, .farg = '.f', .xarg = '.x')
@@ -268,19 +268,19 @@ pro_reduce <- progressively(purrr::reduce, .farg = '.f', .xarg = '.x')
 pro_reduce_right <- progressively(purrr::reduce_right, .farg = '.f', .xarg = '.x')
 #' @rdname pro_map
 #' @export
-pro_reduce2  <- progressively(purrr::reduce2, .farg = '.f', .xarg = '.x', .yarg = '.y')
+pro_reduce2 <- progressively(purrr::reduce2, .farg = '.f', .xarg = '.x', .yarg = '.y')
 #' @rdname pro_map
 #' @export
-pro_reduce2_right  <- progressively(purrr::reduce2_right, .farg = '.f', .xarg = '.x', .yarg = '.y')
+pro_reduce2_right <- progressively(purrr::reduce2_right, .farg = '.f', .xarg = '.x', .yarg = '.y')
 #' @rdname pro_map
 #' @export
-pro_every  <- progressively(purrr::every,  .farg = '.p', .xarg = '.x')
+pro_every <- progressively(purrr::every, .farg = '.p', .xarg = '.x')
 #' @rdname pro_map
 #' @export
-pro_some   <- progressively(purrr::some, .farg = '.p', .xarg ='.x')
+pro_some <- progressively(purrr::some, .farg = '.p', .xarg ='.x')
 #' @rdname pro_map
 #' @export
-pro_accumulate       <- progressively(purrr::accumulate, .farg = '.f', .xarg = '.x' )
+pro_accumulate <- progressively(purrr::accumulate, .farg = '.f', .xarg = '.x' )
 #' @rdname pro_map
 #' @export
 pro_accumulate_right <- progressively(purrr::accumulate_right, .farg = '.f', .xarg = '.x' )
